8ef88f2952a04c4d4c27f9ad47cde34bc6e133e2
aviel1540 <yakovaviel@outlook.co.il>
2022-12-30 21:45:40 +0200

    finish product detailles controller + router

--- server/routers/product_detailes_router.js
+++ server/routers/product_detailes_router.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const productsController = require('../controllers/product_detailes_controller');
+const product_Detailes_Controller = require('../controllers/product_detailes_controller');
 
-router.post("/add", productsController.addNewProduct);
-
+//add new product
+router.post("/add", product_Detailes_Controller.addNewProduct);
+//delete product by id
+router.delete("/delete/:id",product_Detailes_Controller.deleteProduct);
+//show all products
+router.get("/",product_Detailes_Controller.showAllProducts);
+//search product
+router.get("/:id",product_Detailes_Controller.searchProduct);
 
 
 module.exports = router;
